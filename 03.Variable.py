82d8576ba705668ac6ef32d9f2a2de717ea7c015
alicenab <54178612+alicenab@users.noreply.github.com>
2019-09-22 02:49:47 +0400

    Update 03.Variable.py
    changed a concept of code a little bit

--- 03.Variable.py
+++ 03.Variable.py
@@ -1,16 +1,6 @@
-name1="George Hurt"
-name2=str("Burkay Ersan")
-name3=str(input("Please Enter a Name"))
-
-
-age1=126
-age2=int(21)
-age3=int(input("Please Enter a Age"))
-
-length1=1.74
-length2=float(1.70)
-length3=float(input("Please Enter a Length"))
-
-print(name1,age1,length1)
-print(name2,age2,length2)
-print(name3,age3,length3)
+stringim = 'Hi there!' #You can use doublequotes too, like "Hi there"
+floatim = 31.6
+boolum = 1 > 2 #Bool have to return True or False.
+listim = ['car', 'plane', 'ship', 'bicycle']
+tupleum = ('one', 'two', 'three')
+dictim = {'letter': 'a', 'number': 'one', 'symbol': '&'}
