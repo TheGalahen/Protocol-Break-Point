14e8b5e44303440d84a5ae13acbc8621bb3671c1
alicenab <54178612+alicenab@users.noreply.github.com>
2019-09-22 02:44:53 +0400

    Update and rename 04. Example Abouth File 1,2 and 3.py to 04. Example about file 1,2 and 3.py
    the filename and input method have been changed.

--- 04. Example about file 1,2 and 3.py
+++ 04. Example about file 1,2 and 3.py
@@ -1,8 +1,6 @@
-number1=int(input("Please enter a number"))
-number2=int(input("Please enter a number again"))
+number1=30
+number2=20
 number3=number1+number2;
 result=number3//2
 print("Result is",result )
 print("Result is",number3//2)
-
-
